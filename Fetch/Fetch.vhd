90f8f3b7432acf2e1991f5c99723aa911a063c87
Khaled Moataz <khaledmoataz1999@yahoo.com>
2020-05-31 18:17:32 +0200

    bug fix in fetch stall

--- Fetch/Fetch.vhd
+++ Fetch/Fetch.vhd
@@ -76,13 +76,16 @@ ARCHITECTURE rtl OF Fetch IS
     SIGNAL new_address, curr_address, pc_inc    : std_logic_vector (31 downto 0);
     SIGNAL normal_curr_address                  : std_logic_vector (31 downto 0);
     SIGNAL new_instruction                      : std_logic_vector (15 downto 0);
+    SIGNAL pc_enable                            : std_logic;
 BEGIN
-    PC              : reg_rise GENERIC MAP (32) PORT MAP (stall, clk, rst, new_address, normal_curr_address);
+    PC              : reg_rise GENERIC MAP (32) PORT MAP (pc_enable, clk, rst, new_address, normal_curr_address);
     -- instruction_reg : reg GENERIC MAP (16) PORT MAP ('1', clk, rst, new_instruction, out_instruc);
     bpram           : branch_prediction_ram PORT MAP (clk, rst, branch_we, branch_prediction_address, zero_flag, branch_prediction_out);
     instruction_mem : memory GENERIC MAP(16, 16, 11) PORT MAP ('0', '0', curr_address(10 downto 0), "0000000000000000", new_instruction);
     PC_Adder        : adder GENERIC MAP (32) PORT MAP (curr_address,  "00000000000000000000000000000000", '1', open, pc_inc);
     
+    pc_enable <= NOT stall;
+
     branch_prediction_address <= jz_address WHEN jz_signal = '1'
     ELSE curr_address(7 downto 0);
 
