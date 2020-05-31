4564d016980d1e330bf863a85fd49b0a5cf789a6
Khaled Moataz <khaledmoataz1999@yahoo.com>
2020-05-31 21:53:46 +0200

    hazard detection: modifying control signals of swap, adding delivery test cases folder

--- Hazard Detection Unit/Hazard_Detection_Unit.vhd
+++ Hazard Detection Unit/Hazard_Detection_Unit.vhd
@@ -10,8 +10,8 @@ ENTITY Hazard_Detection_Unit IS
             d_alu_src2      : in std_logic;
             d_rdst_wb       : in std_logic;
             d_rdst          : in std_logic_vector(2 downto 0);
-            d_rsrc2         : in std_logic_vector(2 downto 0);
-            d_rsrc2_enable  : in std_logic;
+            d_rsrc1         : in std_logic_vector(2 downto 0);
+            d_swap          : in std_logic_vector(1 downto 0);
             ex_mem_read     : in std_logic;
             ex_mem_op       : in std_logic;
             ex_rdst         : in std_logic_vector(2 downto 0);
@@ -34,7 +34,7 @@ ARCHITECTURE rtl OF Hazard_Detection_Unit IS
     SIGNAL m_pop, instruc_hazard    : std_logic;
     
 BEGIN
-    D_error <= '1' WHEN (((br_reg = d_rdst) OR ((br_reg = d_rsrc2) AND (d_rsrc2_enable = '1'))) 
+    D_error <= '1' WHEN (((br_reg = d_rdst) OR ((br_reg = d_rsrc1) AND (d_swap = "01"))) 
                             AND (d_alu_src2 = '1') AND (d_rdst_wb = '1'))
     ELSE '0';
 
@@ -47,11 +47,10 @@ BEGIN
     m_pop <= '1' WHEN (m_mem_read = '1' AND m_mem_op = '1' AND m_stack = "10")
     ELSE '0';
 
-    instruc_hazard <= '1' WHEN (ex_rsrc1_enable = '1' AND (ex_rsrc1 = m_rdst)) OR
-                        (ex_rsrc2_enable = '1' AND (ex_rsrc2 = m_rdst))
+    instruc_hazard <= '1' WHEN ((ex_rsrc1_enable = '1' AND (ex_rsrc1 = m_rdst)) OR
+                        (ex_rsrc2_enable = '1' AND (ex_rsrc2 = m_rdst)))
+                        -- AND ex_mem_op = '0'
     ELSE '0';
     
     fde_stall <= m_pop AND instruc_hazard;
 END rtl;
-
-
