00b2e9369ccb4da95ea136deed6be1c49867e102
Muhammad Sayed <muhammad.mahmoud98@eng-st.cu.edu.eg>
2020-05-31 23:36:53 +0200

    fix bug in fetch_forward

--- Forwarding Unit/forward_fetch.vhd
+++ Forwarding Unit/forward_fetch.vhd
@@ -11,15 +11,15 @@ ENTITY forward_fetch IS
         Rdst_execute : IN std_logic_vector(2 DOWNTO 0);
         Rdst_execute_en : IN std_logic;
         Rdst_execute_val : IN std_logic_vector(n - 1 DOWNTO 0);
-        Rsrc2_execute : IN std_logic_vector(2 DOWNTO 0);
-        Rsrc2_execute_en : IN std_logic;
-        Rsrc2_execute_val : IN std_logic_vector(n - 1 DOWNTO 0);
+        Rsrc1_execute : IN std_logic_vector(2 DOWNTO 0);
+        execute_opType : IN std_logic_vector (1 downto 0);
+        Rsrc1_execute_val : IN std_logic_vector(n - 1 DOWNTO 0);
         Rdst_memory : IN std_logic_vector(2 DOWNTO 0);
         Rdst_memory_en : IN std_logic;
         Rdst_memory_val : IN std_logic_vector(n - 1 DOWNTO 0);
-        Rsrc2_memory : IN std_logic_vector(2 DOWNTO 0);
-        Rsrc2_memory_en : IN std_logic;
-        Rsrc2_memory_val : IN std_logic_vector(n - 1 DOWNTO 0);
+        Rsrc1_memory : IN std_logic_vector(2 DOWNTO 0);
+        memory_opType : IN std_logic_vector (1 downto 0);
+        Rsrc1_memory_val : IN std_logic_vector(n - 1 DOWNTO 0);
         forwarded_Rdst_en : OUT std_logic;
         forwarded_Rdst_val : OUT std_logic_vector(n - 1 DOWNTO 0));
 END ENTITY forward_fetch;
@@ -28,9 +28,9 @@ ARCHITECTURE forward_fetch_rtl OF forward_fetch IS
 BEGIN
     forwarded_Rdst_en <= '1' WHEN (((Rdst_decode_en = '1') AND (Rdst_fetch = Rdst_decode))
         OR ((Rdst_execute_en = '1') AND (Rdst_fetch = Rdst_execute))
-        OR ((Rsrc2_execute_en = '1') AND (Rdst_fetch = Rsrc2_execute))
+        OR ((execute_opType = "01") AND (Rdst_fetch = Rsrc1_execute))   --swap
         OR ((Rdst_memory_en = '1') AND (Rdst_fetch = Rdst_memory))
-        OR ((Rsrc2_memory_en = '1') AND (Rdst_fetch = Rsrc2_memory)))
+        OR ((memory_opType = "01") AND (Rdst_fetch = Rsrc1_memory)))    --swap
         ELSE
         '0';
 
@@ -38,10 +38,10 @@ BEGIN
         ELSE
         Rdst_execute_val WHEN ((Rdst_execute_en = '1') AND (Rdst_fetch = Rdst_execute))
         ELSE
-        Rsrc2_execute_val WHEN ((Rsrc2_execute_en = '1') AND (Rdst_fetch = Rsrc2_execute))
+        Rsrc1_execute_val WHEN ((execute_opType = "01") AND (Rdst_fetch = Rsrc1_execute))
         ELSE
         Rdst_memory_val WHEN ((Rdst_memory_en = '1') AND (Rdst_fetch = Rdst_memory))
         ELSE
-        Rsrc2_memory_val WHEN ((Rsrc2_memory_en = '1') AND (Rdst_fetch = Rsrc2_memory));
+        Rsrc1_memory_val WHEN ((memory_opType = "01") AND (Rdst_fetch = Rsrc1_memory));
 
 END forward_fetch_rtl;
